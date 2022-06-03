4bb90db04581ee70079159534821622f227dd978
Santana20 <sstfmscontreras@gmail.com>
2022-06-02 22:44:08 -0500

    terminado: 2.1: Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -25,32 +25,40 @@ const Total = ({ course }) => {
   return <p>Number of exercises {total}</p>;
 };
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      {/* <Total course={course} /> */}
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
