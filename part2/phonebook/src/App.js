d555ee11ed6424ff1226537ac6d785fee246de1f
Santana20 <sstfmscontreras@gmail.com>
2022-06-03 23:55:46 -0500

    terminado: 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Input = ({ input }) => {
   return <input value={input.value} onChange={input.handleChange} />;
@@ -39,12 +40,7 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
@@ -74,6 +70,13 @@ const App = () => {
     setNewName("");
     setNewNumber("");
   };
+
+  // use effect
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   // filter array persons with text
   const showPersons =
